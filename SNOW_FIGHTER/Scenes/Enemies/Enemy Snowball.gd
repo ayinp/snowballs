9a89dae73ed3ac1d5c5dc0b89a20b0b8b74c8be3
mattmagnell <mattmagnell@gmail.com>
2023-01-27 03:55:16 +0000

    Copied Level 00 to work on walls, implementing walls and wall prefabs.

--- SNOW_FIGHTER/Scenes/Enemies/Enemy Snowball.gd
+++ SNOW_FIGHTER/Scenes/Enemies/Enemy Snowball.gd
@@ -8,9 +8,9 @@ func _on_VisibilityNotifier2D_screen_exited():
 
 func _on_Enemy_Snowball_body_entered(body):
 	if body == body:
-		print("hit body")
+		#print("hit body")
 		queue_free()
 	if body.name == "Player":
-		print("hit player")
+		#print("hit player")
 		body.health -= 1	
 		queue_free()
